73d5ea8b6eecd1133712275f31fcdbb2167f6b32
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2024-08-28 17:51:59 +0300

    Scripts: Add testbench build for Intel NVL platform DSP
    This patch adds to "scripts/rebuild-testbench.sh -p nvl"
    option to test processing components with NVL DSP build.
    
    Embargo!
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- scripts/set_xtensa_params.sh
+++ scripts/set_xtensa_params.sh
@@ -67,6 +67,11 @@ case "$platform" in
 	XTENSA_CORE="ace30_LX7HiFi4_PIF"
 	TOOLCHAIN_VER="RI-2022.10-linux"
 	;;
+    nvl)
+	PLATFORM="$platform"
+	XTENSA_CORE="ace4px_HiFi5MMU_PIF_nlib"
+	TOOLCHAIN_VER="RI-2022.10-linux"
+	;;
 
     # NXP
     imx8)
@@ -154,7 +159,7 @@ esac
 
 # Pre-zephyr "XTOS" build, testbench,...
 case "$platform" in
-    mtl|lnl|ptl|acp_7_0|mt8196)
+    mtl|lnl|ptl|acp_7_0|mt8196|nvl)
 	SOF_CC_BASE='clang';;
     *)
 	SOF_CC_BASE='xcc';;
