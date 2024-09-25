21a99a1f1b225768425b1a468aeff2b37f2dceda
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2024-09-25 17:56:48 +0300

    Tools: Testbench: Add support for parsing controls from topology
    This adds find or byte controls and set to components after
    initialize. Also other control types mixer, switch, enum are
    parsed but not yet applied to components.
    
    The code is copied and adapted from SOF plugin.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/testbench/include/testbench/topology_ipc4.h
+++ tools/testbench/include/testbench/topology_ipc4.h
@@ -31,6 +31,8 @@ int tb_new_dai_in_out(struct testbench_prm *tp, int dir);
 int tb_new_pga(struct testbench_prm *tp);
 int tb_new_process(struct testbench_prm *tp);
 int tb_pipelines_set_state(struct testbench_prm *tp, int state, int dir);
+int tb_send_bytes_data(struct tb_mq_desc *ipc_tx, struct tb_mq_desc *ipc_rx,
+		       uint32_t module_id, uint32_t instance_id, struct sof_abi_hdr *abi);
 int tb_set_reset_state(struct testbench_prm *tp);
 int tb_set_running_state(struct testbench_prm *tp);
 int tb_set_up_pipeline(struct testbench_prm *tp, struct tplg_pipeline_info *pipe_info);
