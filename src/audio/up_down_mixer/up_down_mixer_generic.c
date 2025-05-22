fe266e4566b944dc64efd766496d915bc3563f21
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2025-05-22 20:17:23 +0300

    Add more stubs processing
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/up_down_mixer/up_down_mixer_generic.c
+++ src/audio/up_down_mixer/up_down_mixer_generic.c
@@ -469,7 +469,7 @@ void downmix32bit_4_0(struct up_down_mixer_data *cd, const uint8_t * const in_da
 
 	while (input_left < end_input_left) {
 		/* Accumulate as Q1.31 * Q1.31 -> Q2.62 */
-		Q_tmp_left += (int64_t)P_coefficient_left * *input_left;
+		Q_tmp_left = (int64_t)P_coefficient_left * *input_left;
 		input_left += 4;
 
 		p = (int64_t)P_coefficient_center * *input_center;
@@ -517,7 +517,7 @@ void downmix32bit_5_0_mono(struct up_down_mixer_data *cd, const uint8_t * const 
 	const uint32_t channel_no = 5;
 
 	for (i = 0; i < in_size / (sizeof(int32_t)); i += channel_no) {
-		Q_tmp += (int64_t)P_coefficient_left * *input_left;
+		Q_tmp = (int64_t)P_coefficient_left * *input_left;
 		input_left += 5;
 
 		Q_tmp += (int64_t)P_coefficient_center * *input_center;
@@ -884,7 +884,27 @@ void downmix16bit_5_1(struct up_down_mixer_data *cd, const uint8_t * const in_da
 void downmix16bit_4ch_mono(struct up_down_mixer_data *cd, const uint8_t * const in_data,
 			   const uint32_t in_size, uint8_t * const out_data)
 {
-	sof_panic(0);
+	int32_t Q_tmp;
+	uint16_t coeffs[4] = {cd->downmix_coefficients[get_channel_index(cd->in_channel_map, 0)],
+			      cd->downmix_coefficients[get_channel_index(cd->in_channel_map, 1)],
+			      cd->downmix_coefficients[get_channel_index(cd->in_channel_map, 2)],
+			      cd->downmix_coefficients[get_channel_index(cd->in_channel_map, 3)]
+			};
+
+	const int16_t *input_data = (const int16_t *)in_data;
+	int16_t *output_data = (int16_t *)out_data;
+	int i;
+
+	for (i = 0; i < in_size / (sizeof(int16_t)); i += 4) {
+		/* Q1.15 * Q.15 -> Q2.30 */
+		Q_tmp = (int32_t)coeffs[0] * input_data[0] +
+			(int32_t)coeffs[1] * input_data[1] +
+			(int32_t)coeffs[2] * input_data[2] +
+			(int32_t)coeffs[3] * input_data[3];
+		*output_data = sat_int16(Q_SHIFT_RND(Q_tmp, 30, 15));
+		input_data += 4;
+		output_data++;
+	}
 }
 
 void downmix32bit_stereo(struct up_down_mixer_data *cd, const uint8_t * const in_data,
@@ -904,37 +924,263 @@ void downmix32bit_stereo(struct up_down_mixer_data *cd, const uint8_t * const in
 void downmix32bit_3_1_mono(struct up_down_mixer_data *cd, const uint8_t * const in_data,
 			   const uint32_t in_size, uint8_t * const out_data)
 {
-	sof_panic(0);
+	int64_t Q_tmp;
+	int32_t P_coefficient_left = cd->downmix_coefficients[CHANNEL_LEFT];
+	int32_t P_coefficient_center = cd->downmix_coefficients[CHANNEL_CENTER];
+	int32_t P_coefficient_right = cd->downmix_coefficients[CHANNEL_RIGHT];
+	int32_t P_coefficient_lfe = cd->downmix_coefficients[CHANNEL_LFE];
+	const int32_t *in32 = (const int32_t *)in_data;
+	const int32_t *input_left = in32 + get_channel_location(cd->in_channel_map, CHANNEL_LEFT);
+	const int32_t *input_center = in32 + get_channel_location(cd->in_channel_map, CHANNEL_CENTER);
+	const int32_t *input_right = in32 + get_channel_location(cd->in_channel_map, CHANNEL_RIGHT);
+	const int32_t *input_lfe = in32 + get_channel_location(cd->in_channel_map, CHANNEL_LFE);
+	int32_t *output = (int32_t *)(out_data);
+	const int channel_no = 4;
+	int i;
+
+	for (i = 0; i < in_size / (sizeof(int32_t)); i += channel_no) {
+		Q_tmp = (int64_t)P_coefficient_left * *input_left;
+		input_left += 4;
+
+		Q_tmp += (int64_t)P_coefficient_center * *input_center;
+		input_center += 4;
+
+		Q_tmp += (int64_t)P_coefficient_right * *input_right;
+		input_right += 4;
+
+		Q_tmp += (int64_t)P_coefficient_lfe * *input_lfe;
+		input_right += 4;
+
+		/* Shift with round to Q1.31 and saturate and store output */
+		*output = sat_int32(Q_SHIFT_RND(Q_tmp, 62, 31));
+		output++;
+	}
 }
 
 void downmix32bit_4_0_mono(struct up_down_mixer_data *cd, const uint8_t * const in_data,
 			   const uint32_t in_size, uint8_t * const out_data)
 {
-	sof_panic(0);
+	int64_t Q_tmp;
+	int32_t P_coefficient_left = cd->downmix_coefficients[CHANNEL_LEFT];
+	int32_t P_coefficient_center = cd->downmix_coefficients[CHANNEL_CENTER];
+	int32_t P_coefficient_right = cd->downmix_coefficients[CHANNEL_RIGHT];
+	int32_t P_coefficient_cs = cd->downmix_coefficients[CHANNEL_CENTER_SURROUND];
+	const int32_t *in32 = (const int32_t *)in_data;
+	const int32_t *input_left = in32 + get_channel_location(cd->in_channel_map, CHANNEL_LEFT);
+	const int32_t *input_center = in32 + get_channel_location(cd->in_channel_map, CHANNEL_CENTER);
+	const int32_t *input_right = in32 + get_channel_location(cd->in_channel_map, CHANNEL_RIGHT);
+	const int32_t *input_cs = in32 + get_channel_location(cd->in_channel_map, CHANNEL_CENTER_SURROUND);
+	int32_t *output = (int32_t *)(out_data);
+	const int channel_no = 4;
+	int i;
+
+	for (i = 0; i < in_size / (sizeof(int32_t)); i += channel_no) {
+		Q_tmp = (int64_t)P_coefficient_left * *input_left;
+		input_left += 4;
+
+		Q_tmp += (int64_t)P_coefficient_center * *input_center;
+		input_center += 4;
+
+		Q_tmp += (int64_t)P_coefficient_right * *input_right;
+		input_right += 4;
+
+		Q_tmp += (int64_t)P_coefficient_cs * *input_cs;
+		input_cs += 4;
+
+		/* Shift with round to Q1.31 and saturate and store output */
+		*output = sat_int32(Q_SHIFT_RND(Q_tmp, 62, 31));
+		output++;
+	}
 }
 
 void downmix32bit_quatro_mono(struct up_down_mixer_data *cd, const uint8_t * const in_data,
 			      const uint32_t in_size, uint8_t * const out_data)
 {
-	sof_panic(0);
+	int64_t Q_tmp;
+	int32_t P_coefficient_left = cd->downmix_coefficients[CHANNEL_LEFT];
+	int32_t P_coefficient_ls = cd->downmix_coefficients[CHANNEL_LEFT_SURROUND];
+	int32_t P_coefficient_right = cd->downmix_coefficients[CHANNEL_RIGHT];
+	int32_t P_coefficient_rs = cd->downmix_coefficients[CHANNEL_RIGHT_SURROUND];
+	const int32_t *in32 = (const int32_t *)in_data;
+	const int32_t *input_left = in32 + get_channel_location(cd->in_channel_map, CHANNEL_LEFT);
+	const int32_t *input_ls = in32 + get_channel_location(cd->in_channel_map, CHANNEL_LEFT_SURROUND);
+	const int32_t *input_right = in32 + get_channel_location(cd->in_channel_map, CHANNEL_RIGHT);
+	const int32_t *input_rs = in32 + get_channel_location(cd->in_channel_map, CHANNEL_RIGHT_SURROUND);
+	int32_t *output = (int32_t *)(out_data);
+	const int channel_no = 4;
+	int i;
+
+	for (i = 0; i < in_size / (sizeof(int32_t)); i += channel_no) {
+		Q_tmp = (int64_t)P_coefficient_left * *input_left;
+		input_left += 4;
+
+		Q_tmp += (int64_t)P_coefficient_ls * *input_ls;
+		input_ls += 4;
+
+		Q_tmp += (int64_t)P_coefficient_right * *input_right;
+		input_right += 4;
+
+		Q_tmp += (int64_t)P_coefficient_rs * *input_rs;
+		input_rs += 4;
+
+		/* Shift with round to Q1.31 and saturate and store output */
+		*output = sat_int32(Q_SHIFT_RND(Q_tmp, 62, 31));
+		output++;
+	}
 }
 
 void downmix32bit_5_1_mono(struct up_down_mixer_data *cd, const uint8_t * const in_data,
 			   const uint32_t in_size, uint8_t * const out_data)
 {
-	sof_panic(0);
+	int64_t Q_tmp;
+	int32_t P_coefficient_left = cd->downmix_coefficients[CHANNEL_LEFT];
+	int32_t P_coefficient_center = cd->downmix_coefficients[CHANNEL_CENTER];
+	int32_t P_coefficient_right = cd->downmix_coefficients[CHANNEL_RIGHT];
+	int32_t P_coefficient_cs = cd->downmix_coefficients[CHANNEL_CENTER_SURROUND];
+	const int32_t *in32 = (const int32_t *)in_data;
+	const int32_t *input_left = in32 + get_channel_location(cd->in_channel_map, CHANNEL_LEFT);
+	const int32_t *input_center = in32 + get_channel_location(cd->in_channel_map, CHANNEL_CENTER);
+	const int32_t *input_right = in32 + get_channel_location(cd->in_channel_map, CHANNEL_RIGHT);
+	const int32_t *input_cs = in32 + get_channel_location(cd->in_channel_map, CHANNEL_CENTER_SURROUND);
+	int32_t *output = (int32_t *)(out_data);
+	const int channel_no = 4;
+	int i;
+
+	for (i = 0; i < in_size / (sizeof(int32_t)); i += channel_no) {
+		Q_tmp = (int64_t)P_coefficient_left * *input_left;
+		input_left += 6;
+
+		Q_tmp += (int64_t)P_coefficient_center * *input_center;
+		input_center += 6;
+
+		Q_tmp += (int64_t)P_coefficient_right * *input_right;
+		input_right += 6;
+
+		Q_tmp += (int64_t)P_coefficient_cs * *input_cs;
+		input_cs += 6;
+
+		/* Shift with round to Q1.31 and saturate and store output */
+		*output = sat_int32(Q_SHIFT_RND(Q_tmp, 62, 31));
+		output++;
+	}
 }
 
 void downmix32bit_7_1_mono(struct up_down_mixer_data *cd, const uint8_t * const in_data,
 			   const uint32_t in_size, uint8_t * const out_data)
 {
-	sof_panic(0);
+	int64_t Q_tmp;
+	int32_t P_coefficient_left = cd->downmix_coefficients[CHANNEL_LEFT];
+	int32_t P_coefficient_center = cd->downmix_coefficients[CHANNEL_CENTER];
+	int32_t P_coefficient_right = cd->downmix_coefficients[CHANNEL_RIGHT];
+	int32_t P_coefficient_cs = cd->downmix_coefficients[CHANNEL_CENTER_SURROUND];
+	const int32_t *in32 = (const int32_t *)in_data;
+	const int32_t *input_left = in32 + get_channel_location(cd->in_channel_map, CHANNEL_LEFT);
+	const int32_t *input_center = in32 + get_channel_location(cd->in_channel_map, CHANNEL_CENTER);
+	const int32_t *input_right = in32 + get_channel_location(cd->in_channel_map, CHANNEL_RIGHT);
+	const int32_t *input_cs = in32 + get_channel_location(cd->in_channel_map, CHANNEL_CENTER_SURROUND);
+	int32_t *output = (int32_t *)(out_data);
+	const int channel_no = 8;
+	int i;
+
+	for (i = 0; i < in_size / (sizeof(int32_t)); i += channel_no) {
+		Q_tmp = (int64_t)P_coefficient_left * *input_left;
+		input_left += 8;
+
+		Q_tmp += (int64_t)P_coefficient_center * *input_center;
+		input_center += 8;
+
+		Q_tmp += (int64_t)P_coefficient_right * *input_right;
+		input_right += 8;
+
+		Q_tmp += (int64_t)P_coefficient_cs * *input_cs;
+		input_cs += 8;
+
+		/* Shift with round to Q1.31 and saturate and store output */
+		*output = sat_int32(Q_SHIFT_RND(Q_tmp, 62, 31));
+		output++;
+	}
 }
 
 void downmix32bit_7_1_to_5_1(struct up_down_mixer_data *cd, const uint8_t * const in_data,
 			     const uint32_t in_size, uint8_t * const out_data)
 {
-	sof_panic(0);
+	int64_t Q_tmp_right_side;
+	int64_t Q_tmp_left_side;
+	uint32_t i;
+
+	channel_map out_channel_map = cd->out_channel_map;
+	const uint8_t left_slot = get_channel_location(out_channel_map, CHANNEL_LEFT);
+	const uint8_t center_slot = get_channel_location(out_channel_map, CHANNEL_CENTER);
+	const uint8_t right_slot = get_channel_location(out_channel_map, CHANNEL_RIGHT);
+	uint8_t right_surround_slot = get_channel_location(out_channel_map, CHANNEL_RIGHT_SURROUND);
+	uint8_t left_surround_slot = get_channel_location(out_channel_map, CHANNEL_LEFT_SURROUND);
+	const uint8_t lfe_slot = get_channel_location(out_channel_map, CHANNEL_LFE);
+
+	/* Must support also 5.1 Surround */
+	const bool surround_5_1_channel_map = (left_surround_slot == CHANNEL_INVALID) &&
+					      (right_surround_slot == CHANNEL_INVALID);
+
+	if (surround_5_1_channel_map) {
+		left_surround_slot = get_channel_location(out_channel_map, CHANNEL_LEFT_SIDE);
+		right_surround_slot = get_channel_location(out_channel_map, CHANNEL_RIGHT_SIDE);
+	}
+
+	int32_t *output_left_ptr = (int32_t *)(out_data + (left_slot << 2));
+	int32_t *output_center_ptr = (int32_t *)(out_data + (center_slot << 2));
+	int32_t *output_right_ptr = (int32_t *)(out_data + (right_slot << 2));
+	int32_t *output_side_left_ptr = (int32_t *)(out_data + (left_surround_slot << 2));
+	int32_t *output_side_right_ptr = (int32_t *)(out_data + (right_surround_slot << 2));
+	int32_t *output_lfe_ptr = (int32_t *)(out_data + (lfe_slot << 2));
+
+	int32_t *in_left_ptr = (int32_t *)in_data;
+	int32_t *in_center_ptr = (int32_t *)(in_data + 4);
+	int32_t *in_right_ptr = (int32_t *)(in_data + 8);
+	int32_t *in_lfe_ptr = (int32_t *)(in_data + 20);
+
+	for (i = 0; i < (in_size >> 5); ++i) {
+		output_left_ptr[i * 6] = in_left_ptr[i * 8];
+		output_right_ptr[i * 6] = in_right_ptr[i * 8];
+		output_center_ptr[i * 6] = in_center_ptr[i * 8];
+		output_lfe_ptr[i * 6] = in_lfe_ptr[i * 8];
+	}
+
+	/* Load the downmix coefficients. */
+	int32_t P_coefficient_left = cd->downmix_coefficients[CHANNEL_LEFT];
+	int32_t P_coefficient_right = cd->downmix_coefficients[CHANNEL_RIGHT];
+	int32_t P_coefficient_left_surround = cd->downmix_coefficients[CHANNEL_LEFT_SURROUND];
+	int32_t P_coefficient_right_surround = cd->downmix_coefficients[CHANNEL_RIGHT_SURROUND];
+
+	const int32_t *in32 = (const int32_t *)in_data;
+	const int32_t *input_left_surround = in32 + get_channel_location(cd->in_channel_map, CHANNEL_LEFT_SURROUND);
+	const int32_t *input_right_surround = in32 + get_channel_location(cd->in_channel_map, CHANNEL_RIGHT_SURROUND);
+	const int32_t *input_left_side = in32 + get_channel_location(cd->in_channel_map, CHANNEL_LEFT_SIDE);
+	const int32_t *input_right_side = in32 + get_channel_location(cd->in_channel_map, CHANNEL_RIGHT_SIDE);
+
+
+	const int32_t *const end_input_left = input_left_surround + (in_size / (sizeof(int32_t)));
+	// const char cs = 8 * sizeof(int32_t);
+
+	while (input_left_surround < end_input_left) {
+		Q_tmp_left_side = (int64_t)P_coefficient_left * *input_left_surround;
+		Q_tmp_right_side = (int64_t)P_coefficient_right_surround * *input_left_surround;
+		input_left_surround += 8;
+
+		Q_tmp_left_side += (int64_t)P_coefficient_left_surround * *input_right_surround;
+		Q_tmp_right_side += (int64_t)P_coefficient_right * *input_right_surround;
+		input_right_surround += 8;
+
+		Q_tmp_left_side += (int64_t)P_coefficient_left * *input_left_side;
+		input_left_side += 8;
+
+		Q_tmp_right_side += (int64_t)P_coefficient_right * *input_right_side;
+		input_right_side += 8;
+
+		/* Shift with round to Q1.31 and saturate and store output */
+		*output_side_left_ptr = sat_int32(Q_SHIFT_RND(Q_tmp_left_side, 46, 31));
+		*output_side_right_ptr = sat_int32(Q_SHIFT_RND(Q_tmp_right_side, 46, 31));
+		output_side_left_ptr += 6;
+		output_side_right_ptr += 6;
+	}
 }
 
 void upmix32bit_4_0_to_5_1(struct up_down_mixer_data *cd, const uint8_t * const in_data,
