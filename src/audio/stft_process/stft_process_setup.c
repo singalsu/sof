fc4f6942f13ca11f5fcebe77e1346e1346d5cdbc
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2026-01-15 22:31:14 +0200

    Math: Add complex to polar and back conversions
    WIP
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/stft_process/stft_process_setup.c
+++ src/audio/stft_process/stft_process_setup.c
@@ -163,6 +163,9 @@ int stft_process_setup(struct processing_module *mod, int max_frames,
 		goto free_fft_buf;
 	}
 
+	/* Use same buffer, but be careful*/
+	fft->fft_polar = (struct ipolar32 *)fft->fft_out;
+
 	fft->fft_fill_start_idx = 0; /* From config pad_type */
 
 	/* Setup FFT */
