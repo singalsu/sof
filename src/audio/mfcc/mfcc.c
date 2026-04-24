b1b95f790425ebd55a53ed7327c6879e28205436
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2026-04-24 18:00:02 +0300

    audio: mfcc: align code style and patterns with stft_process
    Clean up MFCC component code to match improvements done in the
    STFT process module:
    
    - Remove unnecessary includes already provided via mfcc_comp.h
    - Add __cold attribute and assert_can_be_cold() to init and free
    - Add Doxygen comments to all component functions
    - Change log levels from comp_info to comp_dbg for frequently
      called functions (prepare, reset, free, get/set config)
    - Remove redundant debug logs from the hot processing path
    - Fix use-after-free bug in mfcc_free() where mfcc_free_buffers()
      was called after mod_free(cd), reorder to free buffers first
    - Add mfcc_free_buffers() call in mfcc_reset() to properly return
      the component to init-like state when pipeline is stopped
    - Remove stale TODO comment
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/mfcc/mfcc.c
+++ src/audio/mfcc/mfcc.c
@@ -10,27 +10,7 @@
 #include <sof/audio/data_blob.h>
 #include <sof/audio/buffer.h>
 #include <sof/audio/format.h>
-#include <sof/audio/pipeline.h>
-#include <sof/audio/ipc-config.h>
-#include <sof/common.h>
-#include <rtos/panic.h>
-#include <sof/ipc/msg.h>
-#include <sof/lib/uuid.h>
-#include <sof/list.h>
-#include <sof/platform.h>
-#include <sof/ut.h>
-#include <sof/trace/trace.h>
-#include <ipc/control.h>
-#include <ipc/stream.h>
-#include <ipc/topology.h>
-#include <user/mfcc.h>
-#include <user/trace.h>
 #include <rtos/init.h>
-#include <rtos/string.h>
-#include <errno.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 
 LOG_MODULE_REGISTER(mfcc, CONFIG_SOF_LOG_LEVEL);
 
@@ -38,27 +18,26 @@ SOF_DEFINE_REG_UUID(mfcc);
 
 __cold_rodata const struct mfcc_func_map mfcc_fm[] = {
 #if CONFIG_FORMAT_S16LE
-	{SOF_IPC_FRAME_S16_LE,  mfcc_s16_default},
+	{SOF_IPC_FRAME_S16_LE, mfcc_s16_default},
 #endif /* CONFIG_FORMAT_S16LE */
 #if CONFIG_FORMAT_S24LE
 	{SOF_IPC_FRAME_S24_4LE, NULL},
 #endif /* CONFIG_FORMAT_S24LE */
 #if CONFIG_FORMAT_S32LE
-	{SOF_IPC_FRAME_S32_LE,  NULL},
+	{SOF_IPC_FRAME_S32_LE, NULL},
 #endif /* CONFIG_FORMAT_S32LE */
 };
 
-static mfcc_func mfcc_find_func(enum sof_ipc_frame source_format,
-				enum sof_ipc_frame sink_format,
-				const struct mfcc_func_map *map,
-				int n)
+static mfcc_func mfcc_find_func(enum sof_ipc_frame source_format, enum sof_ipc_frame sink_format,
+				const struct mfcc_func_map *map, int n)
 {
 	int i;
 
 	/* Find suitable processing function from map. */
 	for (i = 0; i < n; i++) {
-		if (source_format == map[i].source)
+		if (source_format == map[i].source) {
 			return map[i].func;
+		}
 	}
 
 	return NULL;
@@ -68,17 +47,30 @@ static mfcc_func mfcc_find_func(enum sof_ipc_frame source_format,
  * End of MFCC setup code. Next the standard component methods.
  */
 
-static int mfcc_init(struct processing_module *mod)
+/**
+ * mfcc_init() - Initialize the MFCC component.
+ * @mod: Pointer to module data.
+ *
+ * This function is called when the instance is created. The
+ * macro __cold informs that the code that is non-critical
+ * is loaded to slower but large DRAM.
+ *
+ * Return: Zero if success, otherwise error code.
+ */
+__cold static int mfcc_init(struct processing_module *mod)
 {
 	struct module_data *md = &mod->priv;
 	struct comp_dev *dev = mod->dev;
 	struct mfcc_comp_data *cd = NULL;
 
+	assert_can_be_cold();
+
 	comp_info(dev, "entry");
 
 	cd = mod_zalloc(mod, sizeof(*cd));
-	if (!cd)
+	if (!cd) {
 		return -ENOMEM;
+	}
 
 	/* Handler for configuration data */
 	md->private = cd;
@@ -92,29 +84,64 @@ static int mfcc_init(struct processing_module *mod)
 	return 0;
 }
 
-static int mfcc_free(struct processing_module *mod)
+/**
+ * mfcc_free() - Free dynamic allocations.
+ * @mod: Pointer to module data.
+ *
+ * Component free is called when the pipelines are deleted. All
+ * dynamic allocations need to be freed here. The macro __cold
+ * instructs the build to locate this performance wise non-critical
+ * function to large and slower DRAM.
+ *
+ * Return: Value zero, always success.
+ */
+__cold static int mfcc_free(struct processing_module *mod)
 {
 	struct mfcc_comp_data *cd = module_get_private_data(mod);
 
-	comp_info(mod->dev, "entry");
+	assert_can_be_cold();
+
+	comp_dbg(mod->dev, "free");
+	mfcc_free_buffers(mod);
 	mod_data_blob_handler_free(mod, cd->model_handler);
 	mod_free(mod, cd);
-	mfcc_free_buffers(mod);
 	return 0;
 }
 
-static int mfcc_get_config(struct processing_module *mod,
-			   uint32_t config_id, uint32_t *data_offset_size,
-			   uint8_t *fragment, size_t fragment_size)
+/**
+ * mfcc_get_config() - Handle controls get.
+ * @mod: Pointer to module data.
+ * @config_id: Configuration ID.
+ * @data_offset_size: Size of the whole configuration.
+ * @fragment: Message payload data.
+ * @fragment_size: Size of this fragment.
+ *
+ * Return: Zero if success, otherwise error code.
+ */
+static int mfcc_get_config(struct processing_module *mod, uint32_t config_id,
+			   uint32_t *data_offset_size, uint8_t *fragment, size_t fragment_size)
 {
 	struct sof_ipc_ctrl_data *cdata = (struct sof_ipc_ctrl_data *)fragment;
 	struct mfcc_comp_data *cd = module_get_private_data(mod);
 
-	comp_info(mod->dev, "entry");
+	comp_dbg(mod->dev, "get_config");
 
 	return comp_data_blob_get_cmd(cd->model_handler, cdata, fragment_size);
 }
 
+/**
+ * mfcc_set_config() - Handle controls set.
+ * @mod: Pointer to module data.
+ * @config_id: Configuration ID.
+ * @pos: Position of the fragment in the large message.
+ * @data_offset_size: Size of the whole configuration or offset of fragment.
+ * @fragment: Message payload data.
+ * @fragment_size: Size of this fragment.
+ * @response: Response data.
+ * @response_size: Size of response.
+ *
+ * Return: Zero if success, otherwise error code.
+ */
 static int mfcc_set_config(struct processing_module *mod, uint32_t config_id,
 			   enum module_cfg_fragment_position pos, uint32_t data_offset_size,
 			   const uint8_t *fragment, size_t fragment_size, uint8_t *response,
@@ -122,36 +149,58 @@ static int mfcc_set_config(struct processing_module *mod, uint32_t config_id,
 {
 	struct mfcc_comp_data *cd = module_get_private_data(mod);
 
-	comp_info(mod->dev, "entry");
+	comp_dbg(mod->dev, "set_config");
 
-	return comp_data_blob_set(cd->model_handler, pos, data_offset_size,
-				  fragment, fragment_size);
+	return comp_data_blob_set(cd->model_handler, pos, data_offset_size, fragment,
+				  fragment_size);
 }
 
-static int mfcc_process(struct processing_module *mod,
-			struct input_stream_buffer *input_buffers, int num_input_buffers,
-			struct output_stream_buffer *output_buffers, int num_output_buffers)
+/**
+ * mfcc_process() - The audio data processing function.
+ * @mod: Pointer to module data.
+ * @input_buffers: Pointer to audio input stream buffers.
+ * @num_input_buffers: Number of input buffers.
+ * @output_buffers: Pointer to audio output stream buffers.
+ * @num_output_buffers: Number of output buffers.
+ *
+ * This is the processing function that is called for scheduled
+ * pipelines.
+ *
+ * Return: Zero if success, otherwise error code.
+ */
+static int mfcc_process(struct processing_module *mod, struct input_stream_buffer *input_buffers,
+			int num_input_buffers, struct output_stream_buffer *output_buffers,
+			int num_output_buffers)
 {
 	struct mfcc_comp_data *cd = module_get_private_data(mod);
 	struct audio_stream *source = input_buffers->data;
 	struct audio_stream *sink = output_buffers->data;
 	int frames = input_buffers->size;
 
-	comp_dbg(mod->dev, "start");
-
 	frames = MIN(frames, cd->max_frames);
 	cd->mfcc_func(mod, input_buffers, output_buffers, frames);
 
-	/* TODO: use module_update_buffer_position() from #6194 */
 	input_buffers->consumed += audio_stream_frame_bytes(source) * frames;
 	output_buffers->size += audio_stream_frame_bytes(sink) * frames;
-	comp_dbg(mod->dev, "done");
 	return 0;
 }
 
-static int mfcc_prepare(struct processing_module *mod,
-			struct sof_source **sources, int num_of_sources,
-			struct sof_sink **sinks, int num_of_sinks)
+/**
+ * mfcc_prepare() - Prepare the component for processing.
+ * @mod: Pointer to module data.
+ * @sources: Pointer to audio samples data sources array.
+ * @num_of_sources: Number of sources in the array.
+ * @sinks: Pointer to audio samples data sinks array.
+ * @num_of_sinks: Number of sinks in the array.
+ *
+ * Function prepare is called just before the pipeline is started.
+ * The audio format parameters are retrieved and the processing
+ * function pointer is set.
+ *
+ * Return: Value zero if success, otherwise error code.
+ */
+static int mfcc_prepare(struct processing_module *mod, struct sof_source **sources,
+			int num_of_sources, struct sof_sink **sinks, int num_of_sinks)
 {
 	struct mfcc_comp_data *cd = module_get_private_data(mod);
 	struct comp_buffer *sourceb;
@@ -163,7 +212,7 @@ static int mfcc_prepare(struct processing_module *mod,
 	uint32_t sink_period_bytes;
 	int ret;
 
-	comp_info(dev, "entry");
+	comp_dbg(dev, "prepare");
 
 	/* MFCC component will only ever have 1 source and 1 sink buffer */
 	sourceb = comp_dev_get_first_data_producer(dev);
@@ -179,8 +228,7 @@ static int mfcc_prepare(struct processing_module *mod,
 	/* get sink data format and period bytes */
 	sink_format = audio_stream_get_frm_fmt(&sinkb->stream);
 	sink_period_bytes = audio_stream_period_bytes(&sinkb->stream, dev->frames);
-	comp_info(dev, "source_format = %d, sink_format = %d",
-		  source_format, sink_format);
+	comp_info(dev, "source_format = %d, sink_format = %d", source_format, sink_format);
 	if (audio_stream_get_size(&sinkb->stream) < sink_period_bytes) {
 		comp_err(dev, "sink buffer size %d is insufficient < %d",
 			 audio_stream_get_size(&sinkb->stream), sink_period_bytes);
@@ -214,13 +262,22 @@ err:
 	return ret;
 }
 
+/**
+ * mfcc_reset() - Reset the component.
+ * @mod: Pointer to module data.
+ *
+ * The component reset is called when pipeline is stopped. The reset
+ * should return the component to same state as init.
+ *
+ * Return: Value zero, always success.
+ */
 static int mfcc_reset(struct processing_module *mod)
 {
 	struct mfcc_comp_data *cd = module_get_private_data(mod);
 
-	comp_info(mod->dev, "entry");
+	comp_dbg(mod->dev, "reset");
 
-	/* Reset to similar state as init() */
+	mfcc_free_buffers(mod);
 	cd->mfcc_func = NULL;
 	return 0;
 }
